7ebf7d49bfcdacf6a93aae8c58c6ad95340c2888
tonyabihaidar <tga22@mail.aub.edu>
2025-05-27 15:08:34 +0300

    second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+   async rewrites() {
+       return [
+           {
+               source: '/api/pokemon',
+               destination: 'https://rpr35yaoze.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
+           },
+           {
+               source: '/api/pokemon/:id',
+               destination: 'https://rpr35yaoze.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id',
+           },
+       ];
+   }
 };
+
 
 export default nextConfig;
